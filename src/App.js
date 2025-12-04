462696c9588098b283306793c84a99fdb02ea203
Adrian Villamares <villamares@gmail.com>
2025-12-04 17:24:04 -0600

    Agregar el primer componente

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import ComponenteUno from './components/ComponenteUno';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <ComponenteUno
+          nombre="Adrian"
+          apellido="Villamares"
+          segundoApellido="Martinez"
+        />
       </header>
     </div>
   );
---
+++ src/components/ComponenteUno.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ComponenteUno = (props) => {
+    
+  return (
+    <div>
+      <h1>Componente Uno</h1>
+        <p>Este es el contenido del Componente Uno.</p>
+        <ul>
+            <li>Nombre: <strong>{props.nombre}</strong></li>
+            <li>Apellido Paterno: <strong>{props.apellido}</strong></li>
+            <li>Apellido Materno: <strong>{props.segundoApellido}</strong></li>
+        </ul>
+    </div>
+  )
+}
+
+export default ComponenteUno
